dbafe6126134a45b6af99f0a344ce0c9941f3f21
lorenzodamora <damora.lorenzo.studente@itispaleocapa.it>
2023-05-05 22:43:47 +0200

    Aggiungere i file di progetto.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,20 @@
+﻿//aggiungi progetto alla soluzione > libreria di classi > il namespace è il futuro using > aggiungi riferimento > progetto
+namespace ClassLibrary1
+{
+	public static class Class1
+	{
+		public static bool Pari(this int num) //this int? // '?' significa che può essere null
+		{
+			return num%2==0;
+		}
+	}
+
+	public class Class2
+	{
+		// restituisce il codice della partita IVA
+		public bool Odd(int num)
+		{
+			return num%2==1;
+		}
+	}
+}
---
+++ funzioni esterne/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ClassLibrary1;
+
+namespace funzioni_esterne
+{
+	internal class Program
+	{
+		static void Main()
+		{
+			Console.WriteLine(int.Parse(Console.ReadLine()).Pari());
+			//Console.WriteLine(int.Parse(Console.ReadLine())  .Class1.Pari()); //non funziona perché pari ha un   this int
+			Console.WriteLine(Class1.Pari(int.Parse(Console.ReadLine())));
+			//Console.WriteLine(  Pari  (int.Parse(Console.ReadLine()))); //non funziona perché pari ha bisogno di un   this int
+
+			Class2 class2 = new Class2();
+			//Class1 class1 = new Class1(); //non funziona perché statico
+
+			Console.WriteLine(class2.Odd(int.Parse(Console.ReadLine())));
+			//Console.WriteLine(  Class2.Odd  (int.Parse(Console.ReadLine()))); //non è statico
+		}
+	}
+}
